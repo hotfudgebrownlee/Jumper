b0c48bf096ec3e4daa1cb134c8ec59ae679c060c
hotfudgebrownlee <jarenbrownlee@gmail.com>
2021-02-06 10:29:58 -0700

    input validation for guesser

--- jumper/game/guesser.py
+++ jumper/game/guesser.py
@@ -1,19 +1,30 @@
-from director import Director
-from breakdown import Breakdown
+# from game.director import Director
 class Guesser:
     def __init__(self):
-        self.track_guess = 0
-        self.director = Director()
-        self.breakdown = Breakdown()
-        self.guess = ''
+        self.track_guess = []
+        # self.director = Director()
+        self.guess_correct = True
+        self.word = "apples"
+        
+    
     def get_input(self):
         guess = input('Guess a letter [a-z]: ')
-        if guess in self.director.word():
-            self.guess = guess
-            self.track_guess += 1
+        if guess in "abcdefghijklmnopqrstuvwxyz":
+            if len(guess) != 1:
+                if guess in self.track_guess:
+                    print(f"You've already guessed {guess}. Try again!")
+                    self.get_input()
+                else:
+                    if guess in self.word:
+                        self.guess_correct = True
+                        self.track_guess.append(guess)
+                    else:
+                        self.guess_correct = False
+                        self.track_guess.append(guess)
         else:
-            self.breakdown.cut_chute()
-            self.track_guess += 1
-            self.get_input
-    def correct_guess(self):
-        pass
+            print("Not a valid guess.")
+            self.get_input()
+
+x = Guesser()
+x.get_input()
+print (x.guess_correct)
